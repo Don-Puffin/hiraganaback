95cedef618dd8748c5f3656d8c70cea3c6cad04b
Don-Puffin <152089547+Don-Puffin@users.noreply.github.com>
2024-03-14 15:11:48 +0000

    hiragana generator can now print both english and hiragana to the terminal, using the 'value' and 'englishValue'

--- kana-crazy/src/main/java/org/example/App.java
+++ kana-crazy/src/main/java/org/example/App.java
@@ -3,24 +3,19 @@ package org.example;
 import org.generator.HiraganaGenerator;
 import org.types.Hiragana;
 
-
 import java.util.Map;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         HiraganaGenerator hiraganaGenerator = new HiraganaGenerator();
         Map<Hiragana, String> generatedHiragana = hiraganaGenerator.hiraganaGenerator(20);
 
-
         System.out.println("Generated Hiragana:");
-        for (String hiragana : generatedHiragana.values()) {
-            System.out.println(hiragana);
+        for (Map.Entry<Hiragana, String> entry : generatedHiragana.entrySet()) {
+            Hiragana hiragana = entry.getKey();
+            String englishValue = hiragana.getEnglishValue();
+            String hiraganaValue = hiragana.getValue();
+            System.out.println( hiraganaValue + englishValue);
         }
     }
 }
